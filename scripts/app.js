9e12776a26f1d632416492b0dd4cfdcc9c03b7eb
Adam Purcell <apurcell88@gmail.com>
2021-11-20 15:42:46 -0600

    Added a few controllers in JS. One to control the game state and one to update the display

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,57 @@
-const Gameboard = ((id) => {
-    const spaces = ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'];
-    const spaceBtns = document.querySelectorAll('[data-space-btn]');
+'use strict'
 
-    function render() {
-        spaces.forEach((space, index) => {
-            spaceBtns[index].innerText = space;
-        });
+const Player = (mark) => {
+    this.mark = mark;
 
-        return spaces;
-    }
+    const getMark = () => mark;
 
     return {
-        render,
+        getMark,
+    }
+}
+
+const Gameboard = (() => {
+    let board = ['', '', '', '', '', '', '', '', ''];
+    const spaceBtns = document.querySelectorAll('[data-space-btn]');
+
+    // function render() {
+    //     board.forEach((space, index) => {
+    //         spaceBtns[index].innerText = space;
+    //     });
+
+    //     // return board;
+    // }
+
+    function setBoard(index, sign) {
+        if (index > board.length) return;
+
+        board[index] = sign;
+    }
+
+    function getBoard() {
+        
+    }
+    
+    // spaceBtns.forEach((space, index) => {
+    //     space.addEventListener('click', (e) => {
+    //         console.log(e);
+    //         space.innerText = markBoard(index, player1.getMark());
+    //     });
+    // });
+
+    return {
+        setBoard,
     }
 })();
 
-const Player = (mark) => {
+const DisplayController = (() => {
 
-}
+})();
+
+const GameController = (() => {
+    const player1 = Player('x');
+    const player2 = Player('o');
+    let round = 1;
+})();
 
 Gameboard.render();
