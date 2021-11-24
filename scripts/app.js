1e1aa36306ebd5dbb5e286b628f2f7501d0214e2
Adam Purcell <apurcell88@gmail.com>
2021-11-24 15:13:21 -0600

    Added some function to declare the winner, reset the game on button click. All that's left is to try and set up an AI.

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 'use strict'
-const Player = (mark, name) => {
+const Player = (mark) => {
     this.mark = mark;
     this.name = name;
 
@@ -16,13 +16,17 @@ const DisplayController = (() => {
     const _spaceBtns = document.querySelectorAll('[data-space-btn]');
     const winner = document.querySelector('[data-winner]');
     const winnerContainer = document.querySelector('[data-winner-container]');
+    const newGame = document.querySelector('[data-new-game]');
+    // const displayContainer = document.querySelector('[data-turn-container]');
+    // const displayTurn = document.querySelector('[data-display-turn]');
+
     let board = ['', '', '', '', '', '', '', '', ''];
 
     function render() {
         _spaceBtns.forEach((space, index) => {
             space.addEventListener('mouseup', (e) => {
                 if (board[index] === '') {
-                    space.innerText = GameController.playRound(); 
+                    space.innerText = GameController.getCurrentPlayer(); 
                     board[index] = space.innerText;
                 }
                 checkWin(board);
@@ -31,86 +35,72 @@ const DisplayController = (() => {
     }
 
     function checkWin(board) {
+        // player1 checks
         if (board[0] === 'x' && board[1] === 'x' && board[2] === 'x') {
-            winner.textContent = `${GameController.player1.getName()} wins!`;
-            winnerContainer.appendChild(winner);
+            declareWinnerP1(GameController);
             disableBtns()
         }
         if (board[3] === 'x' && board[4] === 'x' && board[5] === 'x') {
-            winner.textContent = `${GameController.player1.getName()} wins!`;
-            winnerContainer.appendChild(winner);
+            declareWinnerP1(GameController);
             disableBtns()
         }
         if (board[6] === 'x' && board[7] === 'x' && board[8] === 'x') {
-            winner.textContent = `${GameController.player1.getName()} wins!`;
-            winnerContainer.appendChild(winner);
+            declareWinnerP1(GameController);
             disableBtns()
         }
         if (board[0] === 'x' && board[4] === 'x' && board[8] === 'x') {
-            winner.textContent = `${GameController.player1.getName()} wins!`;
-            winnerContainer.appendChild(winner);
+            declareWinnerP1(GameController);
             disableBtns()
         }
         if (board[2] === 'x' && board[4] === 'x' && board[6] === 'x') {
-            winner.textContent = `${GameController.player1.getName()} wins!`;
-            winnerContainer.appendChild(winner);
+            declareWinnerP1(GameController);
             disableBtns()
         }
         if (board[0] === 'x' && board[3] === 'x' && board[6] === 'x') {
-            winner.textContent = `${GameController.player1.getName()} wins!`;
-            winnerContainer.appendChild(winner);
+            declareWinnerP1(GameController);
             disableBtns()
         }
         if (board[1] === 'x' && board[4] === 'x' && board[7] === 'x') {
-            winner.textContent = `${GameController.player1.getName()} wins!`;
-            winnerContainer.appendChild(winner);
+            declareWinnerP1(GameController);
             disableBtns()
         }
         if (board[2] === 'x' && board[5] === 'x' && board[8] === 'x') {
-            winner.textContent = `${GameController.player1.getName()} wins!`;
-            winnerContainer.appendChild(winner);
+            declareWinnerP1(GameController);
             disableBtns()
         }
 
 
+        // player 2 checks
         if (board[0] === 'o' && board[1] === 'o' && board[2] === 'o') {
-            winner.textContent = `${GameController.player2.getName()} wins!`;
-            winnerContainer.appendChild(winner);
+            declareWinnerP2(GameController);
             disableBtns()
         }
         if (board[3] === 'o' && board[4] === 'o' && board[5] === 'o') {
-            winner.textContent = `${GameController.player2.getName()} wins!`;
-            winnerContainer.appendChild(winner);
+            declareWinnerP2(GameController);
             disableBtns()
         }
         if (board[6] === 'o' && board[7] === 'o' && board[8] === 'o') {
-            winner.textContent = `${GameController.player2.getName()} wins!`;
-            winnerContainer.appendChild(winner);
+            declareWinnerP2(GameController);
             disableBtns()
         }
         if (board[0] === 'o' && board[4] === 'o' && board[8] === 'o') {
-            winner.textContent = `${GameController.player2.getName()} wins!`;
-            winnerContainer.appendChild(winner);
+            declareWinnerP2(GameController);
             disableBtns()
         }
         if (board[2] === 'o' && board[4] === 'o' && board[6] === 'o') {
-            winner.textContent = `${GameController.player2.getName()} wins!`;
-            winnerContainer.appendChild(winner);
+            declareWinnerP2(GameController);
             disableBtns()
         }
         if (board[0] === 'o' && board[3] === 'o' && board[6] === 'o') {
-            winner.textContent = `${GameController.player2.getName()} wins!`;
-            winnerContainer.appendChild(winner);
+            declareWinnerP2(GameController);
             disableBtns()
         }
         if (board[1] === 'o' && board[4] === 'o' && board[7] === 'o') {
-            winner.textContent = `${GameController.player2.getName()} wins!`;
-            winnerContainer.appendChild(winner);
+            declareWinnerP2(GameController);
             disableBtns()
         }
         if (board[2] === 'o' && board[5] === 'o' && board[8] === 'o') {
-            winner.textContent = `${GameController.player2.getName()} wins!`;
-            winnerContainer.appendChild(winner);;
+            declareWinnerP2(GameController);
             disableBtns()
         }        
     }
@@ -121,6 +111,37 @@ const DisplayController = (() => {
         });
     }
 
+    function enableBtns() {
+        _spaceBtns.forEach((btn) => {
+            btn.disabled = false;
+        });
+    }
+
+    function declareWinnerP1(Controller) {
+        winner.textContent = `Player ${Controller.player1.getMark()} wins!`;
+        winnerContainer.appendChild(winner);
+    }
+
+    function declareWinnerP2(Controller) {
+        winner.textContent = `Player ${Controller.player2.getMark()} wins!`;
+        winnerContainer.appendChild(winner);
+    }
+
+    function resetGame() {
+        enableBtns();
+        GameController.resetRound();
+        _spaceBtns.forEach((space, index) => {
+            if (board[index] !== '') {
+                    space.innerText = '' 
+                    board[index] = space.innerText;
+            }
+        });
+    
+        winnerContainer.innerText = '';
+    }
+
+    newGame.addEventListener('click', resetGame);
+
     return {
         render,
     }
@@ -129,19 +150,24 @@ const DisplayController = (() => {
 const GameController = (() => {
     const player1 = Player('x', 'Jim');
     const player2 = Player('o', 'Osiris');
-    let _round = 1;
+    let round = 1;
 
-    function playRound() {
+    function getCurrentPlayer() {
         for (let i = 0; i <= 8; i++) {
-            var currentPlayer = _round % 2 === 0 ? player2.getMark() : player1.getMark();
-            _round++;
+            var currentPlayer = round % 2 === 0 ? player2.getMark() : player1.getMark();
+            round++;
         }
         
         return currentPlayer;
     };
 
+    function resetRound() {
+        round = 1;
+    }
+
     return {
-        playRound,
+        getCurrentPlayer,
+        resetRound,
         player1,
         player2,
     }
